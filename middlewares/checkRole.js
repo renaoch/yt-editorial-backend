cd9661b6176798b65bc00fbb0deb4de5a40ed66a
renaoch <chetriprem.work@gmail.com>
2025-04-20 22:05:55 +0530

    Update checkRole.js

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -1,4 +1,5 @@
 const ensureRoleNotAssigned = (req, res, next) => {
+  console.log("req.user in middleware: ",req.user)
   if (req.user && req.user.role) {
     console.log(req.user);
     const redirectUrl = process.env.REDIRECT_URL + "/app";
